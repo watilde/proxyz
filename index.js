b4520d7c74af3630d06423f69f421dcfd0561ed1
Daijiro Wachi <daijiro.wachi@gmail.com>
2014-12-29 02:48:04 +0900

    Init npm and touch index.js

---
+++ index.js
@@ -0,0 +1,41 @@
+var methane = function (receiver, handler) {
+  var Proxy = window.Proxy || false;
+  if (Proxy) {
+    return Proxy(receiver, handler);
+  }
+  var target = Object.create(receiver, {
+    set: {
+      writable: false,
+      enumerable: false,
+      configurable: false,
+      value: function (name, val) {
+        if (!handler.set) {
+          this[name] = val;
+          return true;
+        }
+        return handler.set(target, name, val, receiver);
+      }
+    },
+    get: {
+      writable: false,
+      enumerable: false,
+      configurable: false,
+      value: function (name) {
+        if (!handler.get) {
+          return this[name];
+        }
+        return handler.get(target, name, receiver);
+      }
+    }
+  });
+
+  return target;
+};
+
+var handler = function () {};
+handler.get = function (target, name, receiver) {
+  return target[name];
+};
+var foo = methane({}, handler);
+foo.set('key', 'value');
+foo.get('key'); // => value
